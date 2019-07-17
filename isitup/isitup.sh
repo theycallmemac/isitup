59fe76dcfb541f441d11943c4c800f8e2ca40903
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-07-17 19:02:49 +0000

    remove colours

--- isitup/isitup.sh
+++ isitup/isitup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-if [ -z "$1" ];then echo -e "\e[93m Error \e[39m URL not provided"; else case $(curl -sL -w "%{http_code}\n" $1 -o /dev/null) in 200 ) echo -e "\e[92m ✓ \e[39mUp";; 404 ) echo -e "\e[91m ⦻ \e[39mNot Found";; 000 ) echo -e "\e[91m ⦻ \e[39mNot Found";; 401 ) echo -e "\e[96m ⃠ \e[39mUnauthorized";; * ) echo -e "\e[91m ✗ \e[39mDown";; esac fi
+if [ -z "$1" ];then echo -e " Error: URL not provided"; else case $(curl -sL -w "%{http_code}\n" $1 -o /dev/null) in 200 ) echo -e " ✓ Up";; 404 ) echo -e " ⦻ Not Found";; 000 ) echo -e " ⦻ Not Found";; 401 ) echo -e "  ⃠ Unauthorized";; * ) echo -e " ✗ Down";; esac fi
